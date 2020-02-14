1b608b5ff4b6ec1c8727ea9a7291bb928ee00ea8
Hazim Timimi <timimih@who.int>
2020-02-14 14:08:20 +0100

    Moved non-reactive functions to a separate file (trying to keep tb_profiles_app.R short and readable)

--- tb_profiles_app.R
+++ tb_profiles_app.R
@@ -26,70 +26,8 @@ countries <- year_countries$countries
 rm(year_countries)
 
 
-# Simple reusable function to return label text from a label tag in a dataframe with
-# columns called label_tag and label_text. Note this is a 'non-reactive' function.
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-ltxt <- function(df, tag){
-
-    df[ df$label_tag==tag , "label_text"]
-
-}
-
-# Simple rounding function that returns a string rounded to the nearest integer and
-# uses a space as the thousands separator as per WHO standard.
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-rounder <- function(x) {
-
-    ifelse(is.na(x), NA,
-           formatC(round(x,0), big.mark=" ", format="d")
-           )
-}
-
-
-# Calculate % using numerator and denominator, format the output and cap at 100%
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-display_cap_pct <- function(numerator, denominator) {
-
-  pct <- ifelse(is.na(numerator) | is.na(denominator) | denominator == 0, "",
-         ifelse(numerator > denominator, ">100%", paste0(signif(numerator * 100 / denominator, 2), "%")))
-
-  return(pct)
-}
-
-# Calculate % using numerator and denominator, cap at 100, no formating
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-get_cap_pct <- function(numerator, denominator) {
-
-  pct <- ifelse(is.na(numerator) | is.na(denominator) | denominator == 0, "",
-         ifelse(numerator > denominator, 100, signif(numerator * 100 / denominator, 2)))
-
-  return(pct)
-}
-
-
-# Non-reactive functions to build estimate strings
-# as best (lo-hi)
-format_estimate <- function(best, lo, hi, style="n"){
-
-    if (style=="k"){
-        # format number using spaces for thousands separator
-
-        return(paste0(rounder(best), " (", rounder(lo), "-", rounder(hi), ")"))
-
-    } else if (style=="%") {
-        # append percent sign to best
-        return(paste0(best, "% (", lo, "-", hi, ")"))
-
-    } else {
-        # keep numbers as they are
-
-        return(paste0(best, " (", lo, "-", hi, ")"))
-    }
-
-}
+# Load general, non-reactive functions
+source("general_functions.R")
 
 # Load the standard themes
 source("set_plot_theme.R")
